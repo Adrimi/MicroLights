dae16003c5f5d54f9e0216786ab714aa5ecb26e5
Adrian Szymanowski <adriano100417@gmail.com>
2021-04-26 15:52:27 +0200

    Remove obsolete print statements

--- ESP8266/NeopixelLightController.cpp
+++ ESP8266/NeopixelLightController.cpp
@@ -9,24 +9,20 @@ NeopixelLightController::NeopixelLightController(Adafruit_NeoPixel &pixels) : pi
 
 void NeopixelLightController::clear()
 {
-  Serial.println("clear command");
   pixels.clear();
 }
 
 void NeopixelLightController::show()
 {
-  Serial.println("show command");
   pixels.show();
 }
 
 void NeopixelLightController::setColor(int index, RGB color)
 {
-  Serial.println("setColor commmand | " + (String)index + " : " + (String)color.r + ", " + (String)color.g + ", " + (String)color.b + ".");
   pixels.setPixelColor(index, color.g, color.r, color.b);
 }
 
 void NeopixelLightController::setBrightness(int level)
 {
-  Serial.println("set brightness command");
   pixels.setBrightness(level);
 }
