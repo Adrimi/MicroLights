46f54160904a25ae75e1d381ee178930813a535d
Adrian Szymanowski <adriano100417@gmail.com>
2021-05-05 13:46:34 +0200

    Initialize `MicroLights` project

---
+++ MicroLights/MicroLights/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MicroLights
+//
+//  Created by Adrian Szymanowski on 05/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Button("Select me") {
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MicroLights/MicroLights/MicroLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicroLightsApp.swift
+//  MicroLights
+//
+//  Created by Adrian Szymanowski on 05/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct MicroLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
