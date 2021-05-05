994097b152edf6dd18964ee1656705cccd2626a8
Adrian Szymanowski <adriano100417@gmail.com>
2021-05-05 15:47:57 +0200

    Simple list of devices

--- MicroLights/MicroLights/ContentView.swift
+++ MicroLights/MicroLights/ContentView.swift
@@ -6,17 +6,51 @@
 //
 
 import SwiftUI
+import Combine
+
+struct ConnectableDevice: Identifiable {
+    let id: UUID
+    let name: String
+}
+
+class ContentViewStore: ObservableObject {
+    @Published var connectableItems: [ConnectableDevice]
+    let title: String
+    
+    init(title: String, items: [ConnectableDevice]) {
+        self.connectableItems = items
+        self.title = title
+    }
+}
 
 struct ContentView: View {
+    
+    let store: ContentViewStore
+    
     var body: some View {
-        Button("Select me") {
-            
+        NavigationView {
+            List {
+                ForEach(store.connectableItems) { device in
+                    Text("\(device.name)")
+                }
+            }
+            .listStyle(InsetGroupedListStyle())
+            .navigationTitle(store.title)
         }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(
+            store: ContentViewStore(
+                title: "List of devices",
+                items: [
+                    ConnectableDevice(id: UUID(), name: "esp8266"),
+                    ConnectableDevice(id: UUID(), name: "esp8266"),
+                ]
+            )
+        )
     }
 }
--- MicroLights/MicroLights/MicroLightsApp.swift
+++ MicroLights/MicroLights/MicroLightsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MicroLightsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(store: ContentViewStore(title: "List of items", items: []))
         }
     }
 }
